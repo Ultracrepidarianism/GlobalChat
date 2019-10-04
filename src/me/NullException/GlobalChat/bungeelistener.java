5744f9dfac956cd01a43b75b4c96ff2d22d7c7c8
GAMER <GAMER@192.168.2.10>
2019-10-04 17:47:16 -0400

    2019-10-04 17:47

--- src/me/NullException/GlobalChat/bungeelistener.java
+++ src/me/NullException/GlobalChat/bungeelistener.java
@@ -1,15 +1,17 @@
 package me.NullException.GlobalChat;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.UUID;
 
-import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
-import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
@@ -20,7 +22,7 @@ public class bungeelistener implements Listener {
 	private String server;
 	private Configuration cfg;
 	private File CfgFile;
-
+	public Statement statement;
 	@EventHandler
 	public void onMessage(ChatEvent e) {
 		if(e.getSender() instanceof ProxiedPlayer)
@@ -36,13 +38,32 @@ public class bungeelistener implements Listener {
 			e.setCancelled(true);
 			ProxiedPlayer player = (ProxiedPlayer)e.getSender();
 			String playerName = player.getDisplayName();
-			main.instance.getLogger().info(playerName);
-			String test = cfg.getString(player.getServer().getInfo().getName());
-			test = test.replaceAll("&", "§");
-			main.instance.getProxy().broadcast(new TextComponent(test + playerName + ": " + e.getMessage()));
+			String playerPrefix = "";
+			ResultSet results;
+			try {
+				PreparedStatement statement = main.instance.getConnection().prepareStatement("SELECT p.value "
+						+ "FROM permissions_inheritance pi "
+						+ "INNER JOIN permissions p "
+						+ "ON pi.child = ? AND pi.parent = p.name AND p.permission = 'prefix'");
+				statement.setString(1, player.getUniqueId().toString());
+				results = statement.executeQuery();
+				if(results.next())
+				{
+					playerPrefix = results.getString(1);
+				}
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				return;
+			}
+			playerPrefix = playerPrefix.replaceAll("&", "§");
+			String globalPrefix = cfg.getString("Servers." + player.getServer().getInfo().getName());
+			globalPrefix = globalPrefix.replaceAll("&", "§");
+			main.instance.getProxy().broadcast(new TextComponent(globalPrefix + playerPrefix + playerName + ChatColor.RESET + ": " + e.getMessage()));
 		}
 
 	}
+	
 	
 
 }
--- src/me/NullException/GlobalChat/main.java
+++ src/me/NullException/GlobalChat/main.java
@@ -2,8 +2,13 @@ package me.NullException.GlobalChat;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
 import java.util.Map;
 
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.plugin.Plugin;
@@ -11,33 +16,101 @@ import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
-public class main extends Plugin{
+public class main extends Plugin {
 	public static main instance;
-	public void onEnable()
-	{
+	private Connection connection;
+	public String host, database, username, password;
+	public int port;
+	public void onDisable() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void onEnable() {
 		instance = this;
 		getProxy().getPluginManager().registerCommand(this, new togglechat());
-        getProxy().getPluginManager().registerListener(this, new bungeelistener());
+		getProxy().getPluginManager().registerListener(this, new bungeelistener());
 		if (!main.instance.getDataFolder().exists())
-            main.instance.getDataFolder().mkdir();
-		 
+			main.instance.getDataFolder().mkdir();
+
 		File CfgFile = new File(main.instance.getDataFolder(), "config.yml");
-		
+
 		if (!CfgFile.exists()) {
 			try {
 				CfgFile.createNewFile();
 				Configuration cfg = ConfigurationProvider.getProvider(YamlConfiguration.class).load(CfgFile);
+				cfg.set("Database.host", "localhost");
+				cfg.set("Database.port", 3306);
+				cfg.set("Database.database", "permissions");
+				cfg.set("Database.username", "GlobalChat");
+				cfg.set("Database.password", "password");
 				cfg.set("ToggleChat", "true");
-				for(ServerInfo server : getProxy().getServers().values())
-				{
-					cfg.set(server.getName(), server.getName());
+				for (ServerInfo server : getProxy().getServers().values()) {
+					cfg.set("Servers." + server.getName(), server.getName());
 				}
 				ConfigurationProvider.getProvider(YamlConfiguration.class).save(cfg, CfgFile);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+		} 
+		else {
+			try {
+				Configuration cfg = ConfigurationProvider.getProvider(YamlConfiguration.class).load(CfgFile);
+				if (!cfg.contains("Database")) {
+					cfg.set("Database.host", "localhost");
+					cfg.set("Database.port", 3306);
+					cfg.set("Database.database", "permissions");
+					cfg.set("Database.username", "GlobalChat");
+					cfg.set("Database.password", "password");
+					ConfigurationProvider.getProvider(YamlConfiguration.class).save(cfg, CfgFile);
+				}
+			} 
 			catch (IOException e) {
-				System.out.println("wat");
+				e.printStackTrace();
 			}
 		}
+		mysqlSetup();
 	}
-	
+
+	public void mysqlSetup() {
+		File CfgFile = new File(main.instance.getDataFolder(), "config.yml");
+		Configuration cfg;
+		try {
+			cfg = ConfigurationProvider.getProvider(YamlConfiguration.class).load(CfgFile);
+			host = cfg.getString("Database.host");
+			port = cfg.getInt("Database.port");
+			database = cfg.getString("Database.database");
+			username = cfg.getString("Database.username");
+			password = cfg.getString("Database.password");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+
+			synchronized (this) {
+				if (getConnection() != null && !getConnection().isClosed())
+					return;
+				setConnection(
+						DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database,
+								this.username, this.password));
+				System.out.println("SQL Connected!");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("SQL might not be set up correctly please be sure to modify the config");
+		}
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
 }
